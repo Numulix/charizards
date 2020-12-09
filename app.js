225fe54b54b6d1d7effecb3e44eeaadf2f73a014
Jovan Babic <jbabic999@gmail.com>
2020-12-09 19:20:22 +0100

    Changed to search for a term and display all cards that contain the entered term

--- app.js
+++ app.js
@@ -1,16 +1,37 @@
 // const cardsElement = document.getElementById('cards');
 const cardsElement = document.querySelector('#cards');
+const apiUrl = 'https://api.pokemontcg.io/v1/cards?name=';
 
-const apiUrl = 'https://api.pokemontcg.io/v1/cards?name=charizard';
+const form = document.querySelector('form');
+const input = document.querySelector('input');
+const imageDiv = document.getElementById('cards');
 
-async function getCharizardCards() {
-  const response = await fetch(apiUrl);
-  const json = await response.json();
-  json.cards.forEach((card) => {
-    const image = document.createElement('img');
-    image.src = card.imageUrl;
-    cardsElement.append(image);
-  });
+// Add an event listener on form submission
+form.addEventListener('submit', formSubmission);
+
+function formSubmission(event) {
+  event.preventDefault();
+  imageDiv.innerHTML = '';
+  const term = input.value;
+
+  search(term)
+    .then(displayCards)
 }
 
-getCharizardCards();
+function search(term) {
+  const url = `${apiUrl}${term}`
+
+  return fetch(url)
+    .then(res => res.json())
+    .then(result => {
+      return result.cards;
+    })
+}
+
+function displayCards(cards) {
+  cards.forEach(card => {
+    const imgElement = document.createElement('img');
+    imgElement.src = card.imageUrl;
+    imageDiv.appendChild(imgElement)
+  })
+}
